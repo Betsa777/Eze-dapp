a974d3161c4e81662fa5f2c970e1619d37a57cbc
Betsa777 <dakuyobetsaleel48@gmail.com>
2025-12-23 21:14:33 +0000

    deposit and approve parts

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ approveBtn.addEventListener('click', async () => {
         }
     }
     if (goodUtxo === undefined) {
-        alert("you're not in the officials.")
+        alert("you're not in the officials. Deposit")
         return
     }
     const datum = Data.from(goodUtxo.datum)
